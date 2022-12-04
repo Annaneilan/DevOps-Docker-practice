de4fa78ac72d20f70a8e5a1da1388d80b4aaa0cf
Evgeny <borisenko1315@gmail.com>
2022-12-04 22:56:23 +0200

    standard fix

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-const express = require('express');
+const express = require('express')
 
-const app = express();
-const PORT = process.env.PORT || 8080;
+const app = express()
+const PORT = process.env.PORT || 8080
 
 app.get('/', (request, response) => {
- response.status(200).json({
-   message: 'App is running',
- });
-});
+  response.status(200).json({
+    message: 'App is running'
+  })
+})
 
 app.listen(PORT, () => {
- console.log(`Server is up on localhost:${PORT}`);
-});
+  console.log(`Server is up on localhost:${PORT}`)
+})
